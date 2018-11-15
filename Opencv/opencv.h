14f8d4781fdc52ec673b78811ae6f92f06ce83f4
clipcomet <43530574+clipcomet@users.noreply.github.com>
2018-11-15 11:38:17 +0100

    Update opencv.h

--- Opencv/opencv.h
+++ Opencv/opencv.h
@@ -14,20 +14,24 @@ class Opencv
 {
 public:
     Opencv();
-
-    std::string IntToString(int number);
-    cv::Mat capturevidio(cv::VideoCapture);
-    cv::Mat convertRGBHSV(cv::Mat);
-    cv::Mat Thresholded(cv::Mat);
-    cv::Mat erodedilate(cv::Mat);
-    cv::Point findCenter(cv::Mat);
-    cv::Mat printcenter (cv::Mat,cv::Point);
-    cv::Mat printtextcenter (cv::Mat,cv::Point);
-    cv::Mat vectoroutline (cv::Mat,cv::Mat);
-    void imShow (cv::Mat,std::string);
-    cv::Point allInOne (cv::Mat);
-    void trackBar();
+    cv::Mat capturevidio(cv::VideoCapture cap);
     cv::VideoCapture openVideo(int device);
+    void trackBar();
+    void imShow();
+    cv::Mat convertRGBHSV(cv::Mat imgOriginal);
+    cv::Mat Thresholded(cv::Mat imgHSV);
+
+    cv::Mat Blur(cv::Mat ThrashImg, int x);
+    cv::Mat ImgCanny(cv::Mat BlurImg,cv::Mat Org);
+    cv::Mat Can2Img(cv::Mat org, cv::Mat Can);
+    cv::Point findCenter(cv::Mat imgThresholded);
+    cv::Mat printcenter(cv::Mat imgOriginal, cv::Point center);
+    void allColour(cv::Mat &imgOrg);
+    cv::Mat erodeImg(cv::Mat ThrashImg);
+    cv::Mat dilateImg(cv::Mat ThrashImg);
+    void allSubtrack(cv::Mat &OrgImg);
+    void Both(cv::Mat &OrgImg);
+    void SetFinal(bool Fin);
 
 private:
 
@@ -39,11 +43,21 @@ private:
 
     int iLowV;
     int iHighV;
-    int devMode = 0;
+
+    int lowThreshold = 0;
+    int max_lowThreshold = 100;
+    int scale;
+    bool First = true;
+    cv::Mat StaticImg;
+    int devMode= 2; // 0 == off 1 == Only final pic 2 == all windows
+
+    std::vector<cv::Mat> Matvector{};
+    std::vector<const char*> Matvectorname{};
+    cv::Point subCenter;
+    cv::Point center;
+
 
 
 };
-
-
 
 #endif // OPENCV_H
