a734996e117dd7c03160d133a6ef4c73d3c9e1b3
clipcomet <43530574+clipcomet@users.noreply.github.com>
2018-11-22 08:46:41 +0100

    Update main.cpp

--- Opencv/main.cpp
+++ Opencv/main.cpp
@@ -11,19 +11,43 @@
 
 using namespace std;
 using namespace cv;
+int mode;
 
 int main()
 {
     Opencv c;
     c.trackBar(); // add the trackbar
     VideoCapture cap = c.openVideo(0); // open video for cature
+    cout << "Chose mode"            << endl
+         << "-------------------"   << endl
+         << "1. Both"               << endl
+         << "2.Colour Only"         << endl
+         << "3.Subtracking Only"    << endl
+         << "-------------------"   << endl
+         << "Chosen mode?";
+    cin >> mode;
+
     while(true)
     {
         Mat OrgImg = c.capturevidio(cap); // create Mat from frame of carmara
+        switch(mode) {
 
-        c.Both(OrgImg);  // run both ways og tracking
-        //c.allColour(OrgImg); // run tracking based on HVS
-        //c.allSubtrack(OrgImg); // run tracking base of Subtrackting background
+        case 1 : c.Both(OrgImg);  // run both ways og tracking
+            break;       // and exits the switch
+
+        case 2 : c.allColour(OrgImg); // run tracking based on HVS
+            break;
+
+        case 3 : c.allSubtrack(OrgImg); // run tracking base of Subtrackting background
+            break;
+
+        default:
+            cout << "Wrong choise";
+            break;
+        }
+
+
+
         c.imShow(); // show in console
 
         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
